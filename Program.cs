6ac2d658a3d3e52b79d2a4d5231e7fa0b69950a0
Zbigniew Sieniawski <tesciu68@gmail.com>
2022-03-27 11:38:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MnozeniePrzezDodawanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = Multiply(-3, 7);
+            Console.WriteLine(total);
+        }
+        public static int Multiply(int multiplicand, int factor)
+        {
+            int result = 0;
+            for (int i = 1; i <= Math.Abs(factor); i++)
+            {
+                result = result + multiplicand;    
+            }
+            if (factor > 0)
+            {
+                return result;
+            }
+            else
+            {
+                return -result;
+            }
+        }
+    }
+}
